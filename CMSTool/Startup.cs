d6fb8ec8d08849a268c501033c042a405cb20e76
chris-pal <chris.pal@gmail.com>
2018-09-16 11:20:38 -0400

    fixing page not found

--- CMSTool/Startup.cs
+++ CMSTool/Startup.cs
@@ -31,6 +31,8 @@ namespace CMSTool
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+          app.UseStatusCodePagesWithReExecute("/");
+
           app.UseDefaultFiles();
           app.UseStaticFiles();
           if (env.IsDevelopment())
