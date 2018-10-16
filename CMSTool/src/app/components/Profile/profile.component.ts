971a643e4ddfd3ad453018e5eefa7c4a63666174
chris-pal <chris.pal@gmail.com>
2018-10-16 09:24:32 -0400

    saving user profile

--- CMSTool/src/app/components/Profile/profile.component.ts
+++ CMSTool/src/app/components/Profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ServiceMaster } from '../../services/serviceMaster';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'profile',
@@ -8,12 +9,21 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class ProfileComponent {
   options: FormGroup;
+  router: Router;
 
-
-  constructor(protected svc: ServiceMaster, fb: FormBuilder) {
+  constructor(protected svc: ServiceMaster, fb: FormBuilder, router: Router) {
+    this.router = router;
     this.options = fb.group({
       floatLabel: 'auto',
     });
+  }
 
+  async Save() {
+    this.svc.loginService.UpdateProfile().then(response => {
+    });
+  }
+
+  Cancel() {
+    this.router.navigate(['/home']);
   }
 }
--- CMSTool/src/app/services/LoginService.ts
+++ CMSTool/src/app/services/LoginService.ts
@@ -9,22 +9,25 @@ import { ToastrManager } from 'ng6-toastr-notifications';
 @Injectable()
 export class LoginService {
   isLoggedIn: boolean;
+  url: string;
   public profile: Profile;
 
+
   constructor(protected http: HttpClient, public toastr: ToastrManager) {
+    this.url = 'https://usafacts-api-staging.azurewebsites.net/api/v2/authentication';
     this.isLoggedIn = false;
   }
   login(Username: string, Password: string): Promise<boolean>{
     let header = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-    return this.http.post<Profile>('https://usafacts-api-staging.azurewebsites.net/api/v2/authentication', JSON.stringify({ Username, Password }), { headers: header }).toPromise()
+    return this.http.post<Profile>(this.url, JSON.stringify({ Username, Password }), { headers: header }).toPromise()
       .then(response => {
         this.isLoggedIn = true;
         this.profile = response;
+        //Clear the password field
+        this.profile.Password = String(' ').repeat(8);  //set password to a string of 8 spaces
         this.toastr.successToastr('Welcome ' + response.FirstName + '!', 'Success!', { toastTimeout: 10000 });
         return Promise.resolve(this.isLoggedIn);
-
-
       })
       .catch((error:HttpErrorResponse) => {
         this.isLoggedIn = false;
@@ -37,5 +40,17 @@ export class LoginService {
         return Promise.resolve(this.isLoggedIn);
       });
   }
-  
+
+  public async UpdateProfile(): Promise<boolean> {
+    let header = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post<boolean>(this.url + '/UpdateProfile', JSON.stringify(this.profile), { headers: header }).toPromise()
+      .then(response => {
+        this.toastr.successToastr(this.profile.FirstName + ' your profile has been updated!', 'Success!', { toastTimeout: 10000 });
+        return Promise.resolve(true);
+      })
+      .catch((error: HttpErrorResponse) => {
+          this.toastr.errorToastr('Profile update failed with error: ' + error.message, 'Oops!', { toastTimeout: 10000 });
+          return Promise.resolve(false);
+      });
+  }
 }
